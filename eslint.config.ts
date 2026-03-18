acb5667573aad163d34a4410fa3cb2ee56a6f6fa
AmjadKhateeb <amjadkhateeb99@gmail.com>
2026-03-18 23:05:51 +0200

    chore: ensure CI fails on security warnings

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,9 +1,14 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
+import pluginSecurity from "eslint-plugin-security";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-]);
+export default tseslint.config(
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: { globals: globals.node },
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginSecurity.configs.recommended,
+);
